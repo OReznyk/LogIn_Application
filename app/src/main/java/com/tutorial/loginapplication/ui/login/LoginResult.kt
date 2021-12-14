a4d4887d806b3bdc171497e6969af79b5c4d72c4
OReznyk <oreznyk@outlook.com>
2021-12-14 16:18:21 +0200

    Reusable login app

---
+++ app/src/main/java/com/tutorial/loginapplication/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.tutorial.loginapplication.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
